8acf7a959255b9b38625df8859b758221324ef80
XRMT <2844669010@qq.com>
2025-03-10 16:18:54 +0800

    完成p1
    1.背景长方体循环从左上角向右下角移动
    2.背景小星星与小圆点循环交替闪烁
    3.人物从下方移入

--- projects/250310-CrazyAnimals-First/index.js
+++ projects/250310-CrazyAnimals-First/index.js
@@ -3,18 +3,26 @@ const progressBar = document.getElementById('progress-bar');
 const progressText = document.getElementById('progress-Text');
 // 初始化进度条宽度
 let progressWidth = 0;
+
+// 位置
+let Hmax = 520;
+let Hmin = 0;
+let Wmax = 320;
+let Wmin = 0;
 // 增加进度的函数
 function increaseProgress() {
     
     if (progressWidth < 100) {
-        progressWidth += 1; // 每次增加1%
+        progressWidth += 3; // 每次增加1%
         progressBar.style.width = progressWidth + '%';
         
-        console.log(progressWidth);
+        //console.log(progressWidth);
     } else {
         // 重置进度条宽度并重新开始
         progressWidth = 0;
         progressBar.style.width = progressWidth + '%';
+        clearInterval(intervalId);
+        VisibilityAllP1Elements(1,false)
     }
     progressText.innerText = "已加载"+progressWidth + '%';
 }
@@ -27,4 +35,32 @@ window.onload = function() {
 
     // 设置定时器，每100毫秒调用一次increaseProgress函数
     intervalId = setInterval(increaseProgress, 100);
+
+    patternPos();
 };
+
+
+// pattern位置随机生成
+function patternPos(){
+    for (let index = 1; index < 6; index++) {
+        let id = document.getElementById("pattern-"+index);
+        id.style.top = Math.random() * (Hmax - Hmin) + Hmin + 'px';
+        id.style.left = Math.random() * (Wmax - Wmin) + Wmin + 'px';
+    }
+}
+function randomPosition(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+// 新增函数：隐藏所有 id 为 p1 的元素
+function VisibilityAllP1Elements(id,state) {
+    const p1Elements = document.querySelectorAll('#p'+id);
+    p1Elements.forEach(element => {
+        if(state){
+            element.style.visibility = 'visible';
+        }else{
+            element.style.visibility = 'hidden';
+        }
+    });
+}
